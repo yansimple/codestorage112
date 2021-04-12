cfdc8fbbdc99d06205225d8f15930ce0f02a0f91
yansimple <yandanil2017@yandex.ru>
2021-04-12 15:16:27 +0300

    initial comm

---
+++ teleg_crypto_seller/bot.py
@@ -0,0 +1,21 @@
+import config
+import telebot
+print(config.token)
+bot = telebot.TeleBot(config.token)
+
+@bot.message_handler(commands=["start"])
+def repeat_all_messages(message):
+    keyboard = types.InlineKeyboardMarkup()
+    callback_button1 = types.InlineKeyboardButton(text="1", callback_data="answ1")
+    callback_button2 = types.InlineKeyboardButton(text="1", callback_data="answ1")
+    callback_button3 = types.InlineKeyboardButton(text="1", callback_data="answ1")
+    callback_button4 = types.InlineKeyboardButton(text="1", callback_data="answ1")
+    keyboard.add(callback_button1)
+    keyboard.add(callback_button2)
+    keyboard.add(callback_button3)
+    keyboard.add(callback_button4)
+    bot.send_message(message.chat.id, "hello", reply_markup=keyboard)
+
+
+if __name__ == '__main__':
+    bot.infinity_polling()
